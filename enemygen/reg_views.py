9acf077a06223e05908d2f7b2a0d0fd4aa7d0321
Erkki Lepre <erkki.lepre@iki.fi>
2016-06-18 12:01:22 +0300

    Fixed Reset password view

--- enemygen/reg_views.py
+++ enemygen/reg_views.py
@@ -1,6 +1,6 @@
 from registration.backends.simple.views import RegistrationView
 
 class MyRegistrationView(RegistrationView):
-    def get_success_url(self, request, user):
-        return "/rq_tools/enemygen/"
+    def get_success_url(self, user):
+        return "/mythras_eg/"
         
--- mythras_eg/urls.py
+++ mythras_eg/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from enemygen.reg_views import MyRegistrationView
 from dajaxice.core import dajaxice_autodiscover
 
@@ -11,6 +12,21 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^dajaxice/', include('dajaxice.urls')),
     url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
+
+      url(r'^accounts/password/reset/$',
+                    auth_views.password_reset,
+                    name='password_reset'),
+      url(r'^accounts/password/reset/done/$',
+                    auth_views.password_reset_done,
+                    name='password_reset_done'),
+      url(r'^accounts/password/reset/complete/$',
+                    auth_views.password_reset_complete,
+                    name='password_reset_complete'),
+      url(r'^accounts/password/reset/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
+                    auth_views.password_reset_confirm,
+                    name='password_reset_confirm'),
+
+
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^', include('enemygen.urls')),
     #url(r'^mw_enemygen/', include('mw.urls')),
