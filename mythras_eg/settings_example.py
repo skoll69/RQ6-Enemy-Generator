55a06de4d10b0cb23433a093ed420b982dd1e745
Erkki Lepre <erkki.lepre@iki.fi>
2024-11-10 15:00:37 +0200

    Updated email config in settings_example.py

--- mythras_eg/settings_example.py
+++ mythras_eg/settings_example.py
@@ -80,11 +80,12 @@ LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/accounts/login/'
 
 # Email settings
-EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_USER = 'enemygenerator@gmail.com'
 EMAIL_HOST_PASSWORD = ''
-EMAIL_USE_TLS = False
+EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'enemygenerator@gmail.com'
 SERVER_EMAIL = 'enemygenerator@gmail.com'
 
